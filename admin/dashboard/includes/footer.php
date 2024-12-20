35a583ee782a84cb1e7ea330efee80eb81f55310
Jay-kod <james1ofourtime@gmail.com>
2024-12-20 07:50:00 +0100

    Add files via upload

---
+++ admin/dashboard/includes/footer.php
@@ -0,0 +1,3 @@
+<footer class="main-footer text-center">
+    <i>&copy; <?php echo date("Y"); ?> . <a href="https://dbdconcepts.com.ng">Licensed By dbdconcepts Technology, All Rights Reserved.</a></i>
+</footer>
